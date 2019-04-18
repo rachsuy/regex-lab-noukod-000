5a3933655433641287b63fa20271f01b5e49d46f
Rachele Suy <rachelesuy@gmail.com>
2019-04-18 09:46:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,24 @@
 def starts_with_a_vowel?(word)
-
+word.match(/\A[aeiouAEIOU]/) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+#scan.match(/\bun.\w*ing\b/)
+text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
+  text.scan(/\w{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match?(/\A[+-]?\d+(\.\d+)?\z/)
+  
+ #!text.match?(/[A-Z].*\W$/)
 end
 
 def valid_phone_number?(phone)
+  text.scan(/(\d+)-(\d+)-(\d+)/)
 
 end
